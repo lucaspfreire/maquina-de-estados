1c73275b4a1af6cf6fb3ffd8b3966cf05136bbd6
Lucas Pereira Freire <srlucas.f.pereira@outlook.com>
2025-04-01 23:11:30 -0300

    Criado arquivos iniciais e estipulado critérios para o desenvolvimento do projeto

---
+++ index.js
@@ -0,0 +1,24 @@
+// Máquina de estados / Semáforo
+// Critérios:
+// O sinal vermelho tem que durar 5 segundos;
+// O sinal amarelo tem que durar 2 sefundos;
+// O sinal verde tem que durar 5 segundos;
+
+// Criar botão "Atravessar a rua" com os seguintes critérios:
+// Vermelho: print "Pedestre atravessou a rua!";
+// Amarelo: 30% de chance de ser atropelado e quando atrolepado print "Pedestre foi atropelado!" se não, print "Pedestre atravessou a rua!";
+// Verde: 60% de chance de ser atropelado e quando atrolepado print "Pedestre foi atropelado!" se não, print "Pedestre atravessou a rua!";
+
+// Criar botão "Avançar sinal" com os seguintes critérios:
+// Vermelho: print "Avanço de sinal, carro tomou multa!";
+// Amarelo: 30% de chance do carro tomar multa e quando tomar print "Carro tomou multa!" se não, print "Carro passou sinal!";
+// Verde: print !Carro passou o sinal!";
+
+// Criar contador para as metrícas:
+// Quantos carros passaram o sinal;
+// Quantos carros tomaram multa;
+// Quantos pedestres foram atropelados;
+// Quantos pedestres atravessaram a rua;
+
+// ESTUDAR CONCEITO DE MÁQUINA DE ESTADOS E SET TIMEOUT EM JS
+
